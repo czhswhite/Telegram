9198d15257900da89c3b206cd214f4090a5583c5
czh <czhmdj@163.com>
2019-08-06 16:21:58 +0800

    重新创建

---
+++ config.h
@@ -0,0 +1,14 @@
+//
+//  config.h
+//  Telegraph
+//
+//  Created by 迟中贺 on 2018/9/14.
+//
+
+#ifndef config_h
+#define config_h
+
+#define SETUP_API_ID(apiId) apiId = 255608;
+#define SETUP_API_HASH(apiHash) apiHash = @"ba4dae0d5a268385b52c886c7f0a32ff";
+
+#endif /* config_h */
